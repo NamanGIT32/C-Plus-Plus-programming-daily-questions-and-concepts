4fc10f9376334aa5eb48a3e2d4d71a254ec75139
Naman Paliwal <112724171+NamanGIT32@users.noreply.github.com>
2022-11-06 12:01:27 +0530

    ReverseSentence.cpp

---
+++ Stack/ReverseSentence.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "stack"
+using namespace std;
+void reverse(string s){
+   stack<string> st;
+   for(int i=0;i<s.length();i++){
+      string word=" ";
+      while(s[i]!=' ' && i<s.length()){
+         word=word+s[i];
+         i++;
+      }
+      st.push(word);
+   }
+   while(!st.empty()){
+      cout<<st.top();
+      st.pop();
+   }
+}
+
+int main(){
+   
+   string s="Hello! my name is Naman";
+   reverse(s);
+   return 0;
+}
